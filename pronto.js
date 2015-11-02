c8e64f8cde220cce44579fec9a5403e1617e8973
siddharth <gyrogrid@gmail.com>
2015-11-02 23:00:49 +0530

    second commit: flash to be done

--- pronto.js
+++ pronto.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var credentials = require('./creds.js');
 
 // set up handlebars view engine
 var handlebars = require('express3-handlebars').create();
@@ -9,8 +10,22 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
+app.use(require('body-parser')());
+app.use(require('cookie-parser')(credentials.cookieSecret));
+app.use(require('express-session')());
+app.use(function(req, res, next){
+	// if there's a flash message, transfer
+	// it to the context, then clear it
+	res.locals.flash = req.session.flash;
+	delete req.session.flash;
+	next();
+});
 
 app.get('/', function(req, res){
+	res.render('index');
+});
+
+app.get('/process', function(req, res){
 	res.render('index');
 });
 
